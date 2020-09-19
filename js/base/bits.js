83f3a87461128c5e29723ac3c902b006267246fa
Ivan Cherepanov <mail@av.codes>
2020-09-19 11:58:02 +0300

    feat: more bitwise tests

--- js/base/bits.js
+++ js/base/bits.js
@@ -1,7 +1,36 @@
 module.exports = {
-  s2b: (s) => parseInt(s, 2),
-  b2s: (n) => n.toString(2),
-  bitsCount: (n) => {
-    return Math.min(53, Math.max(1, Math.floor(Math.log2(Math.abs(n)) + 1)));
-  },
+  s2b,
+  b2s,
+  bitsCount,
+  countOnes
 };
+
+
+function s2b(s) {
+  return parseInt(s, 2);
+}
+
+function b2s(n) {
+  return n.toString(2);
+}
+
+function bitsCount(n) {
+  return Math.min(53, Math.max(1, Math.floor(Math.log2(Math.abs(n)) + 1)));
+}
+
+function countOnes(n) {
+  let i = bitsCount(n);
+  let mask = 0b1;
+  let ones = 0;
+
+  while (i > 0) {
+    if (n & mask) {
+      ones++;
+    }
+
+    mask = mask << 1;
+    i--;
+  }
+
+  return ones;
+}
--- js/base/bits.test.js
+++ js/base/bits.test.js
@@ -1,4 +1,4 @@
-const { s2b, b2s, bitsCount } = require("./bits");
+const { s2b, b2s, bitsCount, countOnes } = require("./bits");
 
 describe("s2b", () => {
   it("should parse binary strings", () => {
@@ -109,7 +109,7 @@ describe("bitwise", () => {
     expect(1 ^ 1).toBe(0);
   });
 
-  it("should toggle bits with bitmask", () => {
+  it("should toggle bits with XOR bitmask", () => {
     expect(0b0101 ^ 0b1000).toBe(0b1101);
     expect(0b0011 ^ 0b1000).toBe(0b1011);
     expect(0b0010 ^ 0b0011).toBe(0b0001);
@@ -141,5 +141,106 @@ describe("bitwise", () => {
     expect(b2s(Number.MAX_SAFE_INTEGER).length).toBe(53);
     // However, this one won't be precise anymore
     expect(b2s(Number.MAX_SAFE_INTEGER + 1).length).toBe(54);
+  });
+
+  it("should invert bits with ~", () => {
+    // It also toggles the sign bit
+    expect(~0b0101).toBe(-0b0110);
+
+    // ~x === -(x + 1)
+    expect(~0b1010).toBe(-(0b1010 + 1));
+
+    expect(~0).toBe(-1);
+    expect(~1).toBe(-2);
+    expect(~2).toBe(-3);
+  });
+
+  it("should state if the number is odd with & 1", () => {
+    expect(0 & 1).toBeFalsy();
+    expect(1 & 1).toBeTruthy();
+    expect(2 & 1).toBeFalsy();
+    expect(3 & 1).toBeTruthy();
+  });
+
+  it("should construct operational bitmasks", () => {
+    const src = 0b111000;
+
+    // Toggle
+    expect(src ^ (1 << 2)).toBe(0b111100);
+    expect(src ^ (1 << 3)).toBe(0b110000);
+    expect(src ^ (1 << 4)).toBe(0b101000);
+    expect(src ^ (1 << 5)).toBe(0b011000);
+
+    // Turn strictly off
+    expect(src & ~(1 << 2)).toBe(0b111000);
+    expect(src & ~(1 << 3)).toBe(0b110000);
+    expect(src & ~(1 << 4)).toBe(0b101000);
+    expect(src & ~(1 << 5)).toBe(0b011000);
+
+    // Turn strictly on
+    expect(src | (1 << 2)).toBe(0b111100);
+    expect(src | (1 << 3)).toBe(0b111000);
+    expect(src | (1 << 4)).toBe(0b111000);
+    expect(src | (1 << 5)).toBe(0b111000);
+  });
+
+  it("should swap numbers with three XORs", () => {
+    let a = 0b0011;
+    let b = 0b0101;
+
+    a = a ^ b;
+    b = a ^ b;
+    a = a ^ b;
+
+    expect(a).toBe(0b0101);
+    expect(b).toBe(0b0011);
+  });
+});
+
+describe("countOnes", () => {
+  it("should return count of '1' bits for a given number", () => {
+    expect(countOnes(0b0101)).toBe(2);
+    expect(countOnes(0b0100)).toBe(1);
+    expect(countOnes(0b111111)).toBe(6);
+    expect(countOnes(0b111101)).toBe(5);
+    expect(countOnes(0b0)).toBe(0);
+    expect(countOnes(-0b0)).toBe(0);
+    expect(countOnes(-0b1010)).toBe(2);
+    expect(countOnes(Number.MAX_SAFE_INTEGER)).toBe(32);
+  });
+});
+
+describe("BigInt", () => {
+  it("should allow representing big numbers", () => {
+    expect(b2s(BigInt("1".repeat(256))).length).toBe(848);
+  });
+
+  it("is not strictly equal to Number", () => {
+    expect(0n === 0).toBe(false);
+    expect(0n == 0).toBe(true);
+  });
+
+  it("could be compared to Number as usual", () => {
+    expect(120n > 64).toBe(true);
+    expect(120n < 256).toBe(true);
+    expect(120n <= 120).toBe(true);
+    expect(120n >= 120).toBe(true);
+  });
+
+  it("could be mixed within arrays for sorting", () => {
+    const arr = [4n, 5n, 3, 2, 1, 6n];
+    
+    // BigInt can't be used with Number for arithmetics
+    expect(() => arr.sort((a, b) => a - b)).toThrow();
+
+    // But we still can compare them
+    expect(arr.sort((a, b) => (a < b ? -1 : a > b ? 1 : 0))).toEqual([
+      1,
+      2,
+      3,
+      4n,
+      5n,
+      6n,
+    ]);
   });
 });
